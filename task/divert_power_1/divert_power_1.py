522c7c7df5e40ec13fe4de43bacdfe5dd28c7611
Nikita Panyuhin <79215987151@mail.ru>
2020-12-16 01:30:45 +0300

    Added "Divert Power" (part 1) task

---
+++ task/divert_power_1/divert_power_1.py
@@ -0,0 +1,29 @@
+from Among_Us import execute_action
+
+
+def run(screenshot, task_data):
+
+    leavers_y = 786
+
+    delta = 115
+
+    left_pos, right_pos = float("inf"), float("-inf")
+
+    for x in range(task_data["trigger_region"][0], task_data["trigger_region"][2] + 1):
+        if screenshot.getpixel((x, leavers_y))[0] >= 250:
+            left_pos = min(left_pos, x)
+            right_pos = max(right_pos, x)
+
+    if left_pos == float("inf") or right_pos == float("-inf"):
+        return False
+
+    center_x = (left_pos + right_pos) // 2
+
+    execute_action("mouse_move", center_x, leavers_y)
+    execute_action("mouse_press")
+    execute_action("mouse_move", center_x, leavers_y - delta, 0)
+    execute_action("mouse_release")
+
+    execute_action("wait", 1)
+
+    return True
