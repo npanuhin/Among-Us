f7bfe86a686c7e18ac88ae4efcaf977d9990cf5d
Nikita Panyuhin <79215987151@mail.ru>
2020-12-17 17:55:07 +0300

    Added "Fix Lights" task (sabotage)

---
+++ sabotage/fix_lights/fix_lights.py
@@ -0,0 +1,17 @@
+from Among_Us import execute_action
+
+def run(screenshot, task_data):
+
+    x_positions = (613, 790, 963, 1138, 1312)
+
+    indicators = [(x, 900) for x in x_positions]
+
+    switches = [(x, 784) for x in x_positions]
+
+    for i in range(len(x_positions)):
+        if screenshot.getpixel(indicators[i])[1] <= 150:
+            execute_action("mouse_move", *switches[i])
+            execute_action("mouse_click")
+
+    execute_action("wait", 1)
+    return True
